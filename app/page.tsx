f6790c1942da0fc5b215caa5fc5bb059ded54987
Lukáš Vondra <vondra.ctu@gmail.com>
2024-01-21 22:39:52 +0000

    fixed box shadow

--- app/page.tsx
+++ app/page.tsx
@@ -11,31 +11,33 @@ export default function Home() {
       </section>
 
       {/* SearchForm */}
-      <section className="m-3 mt-0 -mb-[60px] px-2 lg:px-4">
+      <section className="m-3 mt-0 -mb-[32px] px-2 lg:px-4">
         <SearchForm />
       </section>
 
-      <section className="mx-auto max-w-7xl mt-10 p-6 bg-white rounded-lg">
-        <div className="pt-5">
-          <h3 className="text-xl font-bold">Trending destinations</h3>
-          <p className="font-light">Most popular choices for travellers from all around the world</p>
-        </div>
-
-        <div className="flex space-x-4 py-5 overflow-x-scroll">
-        {trending_data.map(item => (
-          <div key={item.id} className="space-y-1 shrink-0 cursor-pointer">
-            <div className="relative w-80 h-72 object-cover rounded-t-lg pb-2">
-              <Image 
-                src={item.src} 
-                alt=""
-                fill
-              />
-            </div>
-            <p className="font-bold">{item.title}</p>
-            <p>{item.location}</p>
-            <p className="font-light text-sm">{item.description}</p>
+      <section className='bg-white py-6'>
+        <div className="mx-auto max-w-7xl mt-6 p-6 bg-white rounded-lg border shadow-lg">
+          <div className="pt-4">
+            <h3 className="text-xl font-bold">Trending destinations</h3>
+            <p className="font-light">Most popular choices for travellers from all around the world</p>
           </div>
-        ))}
+
+          <div className="flex space-x-4 py-5 overflow-x-scroll">
+          {trending_data.map(item => (
+            <div key={item.id} className="space-y-1 shrink-0 cursor-pointer">
+              <div className="relative w-80 h-72 object-cover rounded-t-lg pb-2">
+                <Image 
+                  src={item.src} 
+                  alt=""
+                  fill
+                />
+              </div>
+              <p className="font-bold">{item.title}</p>
+              <p>{item.location}</p>
+              <p className="font-light text-sm">{item.description}</p>
+            </div>
+          ))}
+          </div>
         </div>
       </section>
     </main>
